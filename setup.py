b221d2fc31a44148a05200beacc0f5ce2f478ee3
suthicofi <126922233+suthicofi@users.noreply.github.com>
2024-01-09 06:58:11 -0700

    new update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is our first ai course project',
+    author='Gerald Akorli',
+    license='MIT',
+)
